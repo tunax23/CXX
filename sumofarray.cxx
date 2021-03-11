93f650978afd96416b839e9cc1cdd196cc50060d
Tunax-C <ariestina.tc@gmail.com>
2021-03-11 15:25:25 +0800

    Create sumofarray.cxx

---
+++ sumofarray.cxx
@@ -0,0 +1,38 @@
+/*
+ * sumofarray.cxx
+ * 
+ * Copyright 2021 Tina Chen <ChenTina@10-72-120-211.wifi.8.s.pas.ofs.edu.sg>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int arr[] = {11, 35, 62, 555, 989};
+	int sum = 0;
+	
+	for (int x = 0; x < 5; x++)
+	{
+		sum += arr[x];
+	}
+	cout << sum << endl;
+}
+
